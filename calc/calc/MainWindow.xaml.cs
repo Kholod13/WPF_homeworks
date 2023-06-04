3e9b764bf26a91fbd627a8ab0ee09bc00e1d3ca9
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2023-06-04 15:05:13 +0300

    finish design and start events for buttons

--- calc/calc/MainWindow.xaml.cs
+++ calc/calc/MainWindow.xaml.cs
@@ -20,9 +20,72 @@ namespace calc
     /// </summary>
     public partial class MainWindow : Window
     {
+        enum Operations 
+        { 
+            Add, 
+            Minus, 
+            Mult, 
+            Div, 
+            None = -1,
+        }
+
+        private string previousDigit = string.Empty;
+        private string currentDigit = string.Empty;
+        private int currentOperation = (int)Operations.None;
+        private int lastOperation = (int)Operations.None;
+        private string lastDigit = string.Empty;
         public MainWindow()
         {
             InitializeComponent();
+            TextBoxCurrent.Text = "0";
         }
+
+        //methods
+        public static bool auditZero(string val)
+        {
+            double n;
+            return (double.TryParse(val, out n) && n != 0);
+        }
+
+        public static string DeleteLastDigit(string val)
+        {
+            StringBuilder builder = new StringBuilder(val);
+            if (builder.Length > 1)
+            {
+                builder.Length--;  
+            }
+            else
+            {
+                builder[0] = '0';  
+            }
+            return builder.ToString();
+        }
+
+        //buttons
+        private void ButtonCE_Click(object sender, RoutedEventArgs e)
+        {
+            currentDigit= string.Empty;
+            TextBoxCurrent.Text = "0";
+        }
+
+        private void ButtonC_Click(object sender, RoutedEventArgs e)
+        {
+            currentDigit= string.Empty;
+            previousDigit= string.Empty;
+            TextBoxCurrent.Text = "0";
+            TextBoxHistory.Text = string.Empty;
+            currentOperation = (int)Operations.None;
+        }
+
+        private void ButtonDel_Click(object sender, RoutedEventArgs e)
+        {
+            if (auditZero(currentDigit))
+            {
+                currentDigit = DeleteLastDigit(currentDigit);
+                TextBoxCurrent.Text = currentDigit;
+            }
+        }
+
+
     }
 }
