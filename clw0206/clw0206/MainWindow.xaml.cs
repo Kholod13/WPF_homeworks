98c78b14631c7acee3843a19d0564bad1d683eb2
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2023-06-08 18:26:30 +0300

    do base half

--- clw0206/clw0206/MainWindow.xaml.cs
+++ clw0206/clw0206/MainWindow.xaml.cs
@@ -20,14 +20,70 @@ namespace clw0206
     /// </summary>
     public partial class MainWindow : Window
     {
+        int numPeoples = 0;
         public MainWindow()
         {
             InitializeComponent();
+            buttonOK.IsEnabled= false;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show($"Info\n{name}\n{contact_info}\nType room: {}");
+            string room = "";
+            if (radioButtonEco.IsChecked == true)
+            {
+                room = radioButtonEco.Content.ToString();
+            }
+            else if (radioButtonStandart.IsChecked == true)
+            {
+                room = radioButtonStandart.Content.ToString();
+            }
+            else if (radioButtonLux.IsChecked == true)
+            {
+                room = radioButtonLux.Content.ToString();
+            }
+            List<DateTime> selectedDates = Calendar.SelectedDates.ToList();
+
+            DateTime startDate = selectedDates.Min();
+            DateTime endDate = selectedDates.Max();
+
+            MessageBox.Show($"Info:\nName:{name.Text}\nContact info:{contact_info.Text}\nNum peoples: {num_peoples.Text}\nType room: {room}\nTime: {startDate} to {endDate}");
+        }
+
+        private void RepeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            numPeoples++;
+            if (numPeoples > 12)
+            {
+                numPeoples = 0;
+            }
+            num_peoples.Text = numPeoples.ToString();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            name.Text= string.Empty;
+            contact_info.Text = string.Empty;
+            num_peoples.Text = string.Empty;
+
+            radioButtonEco.IsChecked = false;
+            radioButtonStandart.IsChecked = false;
+            radioButtonLux.IsChecked = false;
+
+            Calendar.SelectedDates.Clear();
+            checkBox.IsChecked = false;
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (checkBox.IsChecked == true)
+            {
+                buttonOK.IsEnabled = true;
+            }
+            if (checkBox.IsChecked == false)
+            {
+                buttonOK.IsEnabled = false;
+            }
         }
     }
 }
