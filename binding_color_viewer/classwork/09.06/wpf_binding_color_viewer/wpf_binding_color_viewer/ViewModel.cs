f0751aa556340c44505ea600db7c14e2a6df39d4
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2023-06-15 14:21:44 +0300

    upload classwork

---
+++ binding_color_viewer/classwork/09.06/wpf_binding_color_viewer/wpf_binding_color_viewer/ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_binding_color_viewer
+{
+    internal class ViewModel
+    {
+    }
+}
